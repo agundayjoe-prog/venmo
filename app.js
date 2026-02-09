c9d7970f5f2b16aaee32edacb886f6ee95822daa
agundayjoe-prog <agundayjoe@gmail.com>
2026-02-09 14:03:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("SW failed", err));
   });
 }
+
+
+// Open popup when clicking any element with this class
+document.querySelectorAll('.show-suspension').forEach(el => {
+  el.addEventListener('click', () => {
+    document.getElementById('suspension-overlay').style.display = 'flex';
+  });
+});
+
+// Close popup
+document.getElementById('closeSuspension').addEventListener('click', () => {
+  document.getElementById('suspension-overlay').style.display = 'none';
+});
